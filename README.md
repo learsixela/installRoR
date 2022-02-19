0ec792281e50a62012458875a9f99025b7a1d01d
israel alexis palma quezada <ipalm001@gmail.com>
2022-02-18 21:54:08 -0300

    cambio version ruby

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # installRoR
 Instalacion Ruby on Rails
+** se actualiza script para las versiones:
+ruby  3.1.0
+rails 7.0.1
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,14 @@
 echo mejorar y actualizar el sistema
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev
 # instalar node.js para ejecutar comandos js al utilizar Rails
 echo instalando node.js para ejecutar JS y Git
 sudo apt-get install -y nodejs
 sudo apt-get install -y git
 # instalar RVM
 echo instalando RVM
-#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 # Instala las llaves públicas de RVM
 sudo apt-key adv –keyserver keyserver.ubuntu.com –recv-keys EBB4CCF6A72BC110
 gpg --keyserver keyserver.ubuntu.com --recv EBB4CCF6A72BC110
@@ -31,15 +31,15 @@ rvm --version
 echo versiones de ruby 
 # rvm list known
 echo instalando Ruby
-rvm install 3.0.0
-rvm use 3.0.0 --default
+rvm install 3.0.1
+rvm use 3.0.1 --default
 # instalar Bundler y Rails
 echo instalando Bundler and Rails
 gem install bundler --no-document
 gem install rails -v 6.1.4 --no-document
 # instalar postgres y sus dependencais
 #echo instalando psql y sus dependencias
-sudo apt-get install -y postgresql postgresql-contrib libpq-dev
+#sudo apt-get install -y postgresql postgresql-contrib libpq-dev
 echo version instalada
 ruby -v
 rails -v
